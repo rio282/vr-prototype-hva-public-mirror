e36b9d0c9e475a8b0bc74c990baae8e567bd7a77
Rio Bérénos <r.berenos@hotmail.com>
2026-03-31 22:57:25 +0200

    Update webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,6 +7,8 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = merge(common, {
 	mode: "production",
 
+	entry: "./js/app.js",
+
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "js/[name].[contenthash].js",
